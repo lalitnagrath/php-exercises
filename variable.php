aa2cc9157b156612f84a97a3ed1284df1a43ae37
Lalit Nagrath <lalitnagrath@gmail.com>
2019-08-28 11:53:46 +0530

    Variable in php

---
+++ variable.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
+<?php 
+
+$name=" Laliz ";
+$age=35;
+
+echo ("There once was a man named $name<br>");
+echo (" he was $age years of age<br>");
+echo (" he really didnt liked his name $name<br>");
+echo ("but he really liked being $age years<br>");
+
+?>
+</body>
+</html>
