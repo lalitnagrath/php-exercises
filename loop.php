bb04fa414a21435c5e355d45a7b32eca50ade3e4
lalit <lalitnagrath@gmail.com>
2020-07-02 12:08:20 +0530

    loop in php

---
+++ loop.php
@@ -0,0 +1,24 @@
+<?php
+
+?>
+<table border="2px">
+<?php
+for($x=1;$x<=100;$x){
+    ?>
+    <tr>
+    <?php
+    for($y=0;$y<=9;$y++){
+        ?> <td>
+        <?php echo $x+$y ."  <br>";
+         ?> </td><?php        
+}
+    $y++;
+   ?>
+   </tr>
+   <?php
+    $x=$x+10;
+}?>
+</table>
+<?php
+
+?>
